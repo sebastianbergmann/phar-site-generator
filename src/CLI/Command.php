a6baa182046a4c2c54388924046aa8c0057986ad
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-01-24 08:00:41 +0100

    Add license information

--- src/CLI/Command.php
+++ src/CLI/Command.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of phar-site-generator.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SebastianBergmann\PharSiteGenerator;
 
 use Symfony\Component\Console\Command\Command as AbstractCommand;
