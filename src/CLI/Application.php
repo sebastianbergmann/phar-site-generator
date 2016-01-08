d8bd9e5199511d72397fd239790f346b4f134bc3
Sebastian Bergmann <sebastian@thephp.cc>
2016-01-08 09:11:06 +0100

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -20,7 +20,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('1.4.1', dirname(dirname(__DIR__)));
+        $version = new Version('1.5.0', dirname(dirname(__DIR__)));
         parent::__construct('phar-site-generator', $version->getVersion());
     }
 
